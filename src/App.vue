a6b04d110e369639fcb46acf7af893d64fa4c9a8
Gregory <g.a.soldatov@gmail.com>
2023-02-28 16:43:24 +0300

    upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <weather-widget/>
+</template>
+
+<script>
+import WeatherWidget from '@/components/WeatherWidget.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherWidget
+  }
+}
+</script>
